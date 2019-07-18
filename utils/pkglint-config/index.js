b024dd20750d8759bdabac9c79d2a57802e4255a
Max Milton <max@wearegenki.com>
2019-07-19 08:26:32 +1000

    Experimental linter

---
+++ utils/pkglint-config/index.js
@@ -0,0 +1,38 @@
+// https://github.com/camacho/format-package#options
+
+'use strict';
+
+module.exports = {
+  order: [
+    'name',
+    'version',
+    'description',
+    'license',
+    'private',
+    'engines',
+    'os',
+    'cpu',
+    'repository',
+    'bugs',
+    'homepage',
+    'author',
+    'keywords',
+    'bin',
+    'man',
+    'main',
+    'directories',
+    'module',
+    'browser',
+    'config',
+    'publishConfig',
+    'scripts',
+    'husky',
+    'lint-staged',
+    '...rest',
+    'dependencies',
+    'peerDependencies',
+    'devDependencies',
+    'optionalDependencies',
+    'bundledDependencies',
+  ],
+};
